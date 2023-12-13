2ac5c39acb8e84b8e4ef3b707212603ff7c8c6fc
Matthew Brooks <ghakazian@comcast.net>
2023-12-13 14:07:24 -0700

    Revert "Second change: Add publisher table and field"

--- books/models.py
+++ books/models.py
@@ -3,8 +3,3 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=255)
     author = models.CharField(max_length=255, null=True)
-    publisher = models.ForeignKey('books.Publisher', on_delete=models.CASCADE, null=True)
-
-
-class Publisher(models.Model):
-    name = models.CharField(max_length=255)
